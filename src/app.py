243df8a02a4b60bed479de919496111a6d721535
takahiro-yamanaka-hh <takahiro.yamanaka.hh@hitachi.com>
2021-07-16 04:53:01 +0900

    問合せフォーム追加

--- src/app.py
+++ src/app.py
@@ -6,8 +6,7 @@ import socket
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    html = "<h3>Hello World2</h3>"
+    html = "<head><title>問合せフォーム</title></head><body><h3>お問い合わせフォーム</h3><form><label>e-mail</label><input ></input></br><label>本文</label><textarea row=\"4\"></textarea></form></body>"
     return html.format()
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
-    # Add Commnent2
