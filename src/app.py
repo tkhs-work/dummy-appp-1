799d8a58001789dd0faa93cbaea0f4c360a41bd7
takahiro-yamanaka-hh <takahiro.yamanaka.hh@hitachi.com>
2021-07-16 05:23:56 +0900

    スタイル適用

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ import socket
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    html = "<head><title>問合せフォーム</title></head><body><h3>お問い合わせフォーム</h3><form><label>e-mail</label><input ></input></br><label>本文</label><textarea row=\"4\"></textarea></form></body>"
+    html = "<head><meta charset=\"UTF-8\"><title>問合せフォーム</title><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\"><link rel=\"stylesheet\" href=\"static/style.css\"></head><body><div class=\"container\"><h3>お問い合わせフォーム</h3><form><label class=\"label\" for=\"e-mail\">e-mail</label><input ></input></br><label class=\"label\" for=\"message\">本文</label><textarea row=\"4\"></textarea><button id=\"btn_submit\">送信する</button></form></div></body>"
     return html.format()
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
